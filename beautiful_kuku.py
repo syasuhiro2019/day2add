0a10347c20e552bdf007eb0a7a33d4fd65ab0a14
Sato <s_ychm@yahoo.co.jp>
2019-03-10 23:42:27 +0900

    9*9までの結果

--- beautiful_kuku.py
+++ beautiful_kuku.py
@@ -1,4 +1,10 @@
-for row in range(1, 3):
-    for col in range(1, 3):
-        varue = row * col
-        print(varue)
+# for j in range(1, 10):
+#     for i in range(1, 10):
+#         print(j * i, end=' ')
+#     print()
+
+for row in range(1, 10):
+    for col in range(1, 10):
+        value = row * col
+        print(f'{col} ✕ {row} = {value} | ', end=' ')
+    print()
