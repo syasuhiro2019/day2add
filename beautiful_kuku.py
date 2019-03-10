5e46e2d4dc4606902c9b6e61d344c61b7c532ae6
Sato <s_ychm@yahoo.co.jp>
2019-03-11 00:21:49 +0900

    main追加

--- beautiful_kuku.py
+++ beautiful_kuku.py
@@ -1,13 +1,12 @@
-# for j in range(1, 10):
-#     for i in range(1, 10):
-#         print(j * i, end=' ')
-#     print()
-#
-Row = int((input('行数を入力してください > ')))
-Column = int((input('桁数を入力してください > ')))
+def main():
+    Row = int((input('行数を入力してください > ')))
+    Column = int((input('桁数を入力してください > ')))
+    for row in range(1, Row + 1):
+        for col in range(1, Column + 1):
+            value = row * col
+            print(f'{col} ✕ {row} = {value} | ', end='   ')
+        print()
 
-for row in range(1, Row + 1):
-    for col in range(1, Column + 1):
-        value = row * col
-        print(f'{col} ✕ {row} = {value} | ', end=' ')
-    print()
+
+if __name__ == '__main__':
+    main()
