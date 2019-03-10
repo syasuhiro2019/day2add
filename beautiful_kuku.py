8af138b677779eaff07b0f48d988f0f6b899f617
Sato <s_ychm@yahoo.co.jp>
2019-03-10 23:57:59 +0900

    9*9までの結果(input追加)

--- beautiful_kuku.py
+++ beautiful_kuku.py
@@ -2,9 +2,12 @@
 #     for i in range(1, 10):
 #         print(j * i, end=' ')
 #     print()
+#
+Row = int((input('行数を入力してください > ')))
+Column = int((input('桁数を入力してください > ')))
 
-for row in range(1, 10):
-    for col in range(1, 10):
+for row in range(1, Row + 1):
+    for col in range(1, Column + 1):
         value = row * col
         print(f'{col} ✕ {row} = {value} | ', end=' ')
     print()
