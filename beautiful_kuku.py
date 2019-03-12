ea62d6bbfee1651b5d23457e612bd3679d985706
Sato <s_ychm@yahoo.co.jp>
2019-03-12 22:22:44 +0900

    12✕12までの整列

--- beautiful_kuku.py
+++ beautiful_kuku.py
@@ -1,15 +1,16 @@
 def main():
     # row = int(input('行数を入力してください > '))
     # column = int(input('桁数を入力してください > '))
-    row = 9
-    column = 9
+    row = 12
+    column = 12
 
     for row in range(1, row + 1):
         for col in range(1, column + 1):
             product = row * col
 
-            padded_product = str(product).rjust(2)
-            print(f'{col} ✕ {row} = {padded_product} | ', end='   ')
+            padded_product = str(product).rjust(3)
+            padded_row = str(row).rjust(2)
+            print(f'{col} ✕ {padded_row} = {padded_product} | ', end='   ')
         print()
 
 
